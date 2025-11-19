1f671a3738e069ec0072e82d557733af42608f53
Kate Saunders <kate.saunders@monash.edu>
2025-11-19 22:11:14 +1100

    Error handling in who_is_the_hottest

--- R/who_is_the_hottest.R
+++ R/who_is_the_hottest.R
@@ -2,19 +2,18 @@ who_is_the_hottest <- function(year, special = NULL){
 
   year_range = hottest100_tracks$pollyear |> range()
 
-  if(year < min(year_range) | year > max(year_range) |
-     (year == 1989 & is.null(special))){
-    stop("Sorry that countdown isn't in this dataset")
-  }
+  if(year < min(year_range) | year > max(year_range))
+    stop("Sorry that countdown isn't in this dataset.")
 
   if(is.null(special)){
 
     countdown_winner = hottest100_tracks |>
       filter(pollyear == year & position == 1 & alltime == FALSE)
 
-    output_str = glue("In {year}, {countdown_winner$artist} was the hottest!")
+    if(nrow(countdown_winner) == 0)
+      stop("Sorry that countdown isn't in this dataset.")
 
-    print(output_str)
+    output_str = glue("In {year}, {countdown_winner$artist} was the hottest!")
 
   }else{
 
@@ -23,10 +22,10 @@ who_is_the_hottest <- function(year, special = NULL){
 
     output_str = glue("For the special countdown in {year} - {countdown_winner$artist} was the hottest!")
 
-    print(output_str)
-
   }
 
-  return(countdown_winner)
+  print(output_str)
+
+  invisible(NULL)
 
 }
