971045f4b6165ad65279afd829637c881d6631f5
Dianne Cook <visnut@gmail.com>
2025-11-20 09:05:01 +1100

    updated data roxygen based on Cynthia's pull request

--- R/data.R
+++ R/data.R
@@ -1,48 +1,45 @@
-#' Information about albums on the TripleJ Hottest 100
+#' Triple J Hottest 100 Album Data
 #'
-#' \itemize{
-#'   \item year
-#'   \item alltime
-#'   \item alltimedescription
-#'   \item colorRGB
-#'   \item contrastColorRGB
+#' A dataset containing album information associated with tracks from the Triple J Hottest 100, scraped on 19 Nov, 2025 from the official Triple J archive.
+#'
+#' @format A data.frame with one row per album, including variables such as:
+#' \describe{
+#'   \item{year}{The year the Hottest 100 was held.}
+#'   \item{alltime}{Logical; whether the ranking is considered for an "All-Time" list.}
+#'   \item{alltimedescription}{A description of the all-time list, if applicable.}
+#'   \item{colorRGB}{Primary RGB color value associated with the album or year in character format.}
+#'   \item{contrastColorRGB}{Contrasting RGB color value (in character format) suitable for text overlay.}
 #' }
-#'
-#' @name albums
-#' @docType data
-#' @format A 42 x 5 data frame
-#' @keywords datasets
-NULL
+#' @source \url{https://www.abc.net.au/triplej/hottest100/archive/search/}
+"albums"
 
-#' Information about artists on the TripleJ Hottest 100
+#' Triple J Hottest 100 Artists
 #'
-#' \itemize{
-#'   \item artist
-#'   \item country
-#'   \item releaseyear
+#' A deduplicated dataset of artists represented in the Triple J Hottest 100, created from \code{hottest100_tracks}.
+#'
+#' @format A data.frame with one row per artist, including variables:
+#' \describe{
+#'   \item{artist}{Artist name}
+#'   \item{country}{Country of origin}
+#'   \item{releaseyear}{Release year of included track}
 #' }
-#'
-#' @name artists
-#' @docType data
-#' @format A 2729 x 3 data frame
-#' @keywords datasets
-NULL
+#' @source Derived from \code{hottest100_tracks}
+"artists"
 
-#' Information about tracks on the TripleJ Hottest 100
+#' Triple J Hottest 100 Track Data
 #'
-#' \itemize{
-#'   \item alltime
-#'.  \item artist
-#'   \item country
-#'   \item id
-#'   \item pollyear
-#'.  \item position
-#'   \item releaseyear
-#'   \item track
+#' A dataset containing track-level data from the Triple J Hottest 100, scraped on 19 Nov, 2025 from the official Triple J archive.
+#'
+#' @format A data.frame with one row per track, including variables such as:
+#' \describe{
+#'   \item{alltime}{Logical; indicates if the entry is from an all-time chart or specific year.}
+#'   \item{artist}{Character; name of the artist or band associated with the track.}
+#'   \item{country}{Character; the country associated with the track/chart.}
+#'   \item{id}{Unique identifier for each track (within year)}
+#'   \item{pollyear}{Countdown poll year}
+#'   \item{position}{Chart position of the track.}
+#'   \item{releaseyear}{Original release year of the track.}
+#'   \item{track}{Title of the track or song.}
 #' }
-#'
-#' @name tracks
-#' @docType data
-#' @format A 4200 x 8 data frame
-#' @keywords datasets
-NULL
+#' @source \url{https://www.abc.net.au/triplej/hottest100/archive/search/}
+"tracks"
