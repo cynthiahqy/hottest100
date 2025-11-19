c32c4d5dd25e09b93593255069ed1797953ecc7a
Kate Saunders <kate.saunders@monash.edu>
2025-11-19 17:21:57 +1100

    Draft version of function

--- R/who_is_the_hottest.R
+++ R/who_is_the_hottest.R
@@ -1,3 +1,21 @@
-who_is_the_hottest <- function(){
-  print("Want to write a silly function that returns the hottest by filter call")
+who_is_the_hottest <- function(year, special = NULL){
+
+  if(is.null(special)){
+
+    countdown_winner = hottest100_tracks |>
+      filter(pollyear == year & position == 1 & alltime == FALSE)
+
+    glue("In {year}, {countdown_winner$artist} was the hottest!") |> print()
+
+  }else{
+
+    countdown_winner = hottest100_tracks |>
+      filter(pollyear == year & position == 1 & alltime == TRUE)
+
+    glue("For the special countdown in {year} - {countdown_winner$artist} was the hottest!") |> print()
+
+  }
+
+  return(countdown_winner)
+
 }
